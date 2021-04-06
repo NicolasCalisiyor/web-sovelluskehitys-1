26d5fe24009da7e46647bc0ca2e4eb71203d32ab
nicalx <c3eo18@gmail.com>
2021-04-06 23:12:19 +0300

    lab 9 / task 2

--- labs/09 Single-Page Applications/js/building_lists.js
+++ labs/09 Single-Page Applications/js/building_lists.js
@@ -7,11 +7,18 @@ console.log(data);
 
 var books = data.books;
 
-var list = document.createElement('ol');
+let header = document.createElement("h1");
+header.innerHTML = "header";
+
+var list = document.createElement('ul');
 for (var i=0; i < books.length; i++) {
 	console.log(books[i].title);
-	var item = document.createElement('li');
-	item.innerHTML = books[i].title;
+	var item = document.createElement('li', onclick = clicked());
+	item.innerHTML = books[i].title + " " + books[i].year;
 	list.appendChild(item);
 }
 document.body.appendChild(list);
+
+function clicked() {
+
+}
--- labs/09 Single-Page Applications/js/simple_dom.js
+++ labs/09 Single-Page Applications/js/simple_dom.js
@@ -2,23 +2,30 @@
 
 console.log(document);
 
-document.getElementById('save').onclick = save;
-document.querySelector('#userForm input[type="email"]').onkeypress = function() {
+
+var data = document.querySelectorAll('#userForm input');
+console.log(data);
+var paragraphs = document.querySelectorAll('#summary p');
+
+//document.getElementById('save').onclick = save;
+document.querySelector('#userForm input[type="email"]').onkeyup = function() {
 	console.log('updating email');
 	var email = document.querySelector('#userForm input[type="email"]').value;
 	document.querySelector('#summary p').innerHTML = email;
 };
 
 
-function save() {
-	console.log('save');
 	// get a DOM object representing a form field.
-	var name = document.querySelector('#userForm input[type="text"]');
-	console.log(name);
-	document.querySelector('#summary h1').innerHTML = name.value;
-	var data = document.querySelectorAll('#userForm input');
-	console.log(data);
-	var paragraphs = document.querySelectorAll('#summary p');
-	console.log('found '+paragraphs.length+' p tags');
-	paragraphs[1].innerHTML = 'Hello World!';
-}
+
+document.querySelector('#userForm input[type="text"]').onkeyup = function () {
+	console.log('updating name');
+	var name = document.querySelector('#userForm input[type="text"]').value;
+	document.querySelector('#summary h1').innerHTML = name;
+};
+
+document.querySelector('#userForm input[type="password"]').onkeyup = function () {
+	console.log('updating password');
+	var pw = document.querySelector('#userForm input[type="password"]').value;
+	paragraphs[1].innerHTML = pw;
+};
+
